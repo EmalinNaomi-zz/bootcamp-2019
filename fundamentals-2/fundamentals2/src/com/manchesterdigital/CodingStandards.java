3b1b760d3c09d237cfeb70b83faf7017457e72ec
Emalin <Emalin>
2019-09-19 17:01:10 +0100

    Added variables and data types.

---
+++ fundamentals-2/fundamentals2/src/com/manchesterdigital/CodingStandards.java
@@ -0,0 +1,16 @@
+package com.manchesterdigital;
+
+/**
+ * This style describes coding standards
+ *
+ * @author MattEm1
+ */
+public class CodingStandards {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+
+
+}
+
---
+++ fundamentals-2/fundamentals2/src/com/manchesterdigital/Variables.java
@@ -0,0 +1,30 @@
+package com.manchesterdigital;
+
+
+import java.util.Currency;
+
+public class Variables {
+
+    public static void main(String[] args) {
+
+        int numberOfAnimals;
+
+        Integer numberOfStudents = null;
+
+        System.out.println(Short.);
+
+        Boolean myBoolean = Boolean.FALSE;
+        Currency
+    }
+
+
+
+
+    }
+
+
+
+
+
+
+}
